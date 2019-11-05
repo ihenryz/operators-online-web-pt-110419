ea1a2151edfb6b288f00fb8d8ea4ac9de8c9a2ea
Henry Phan <h.phan06@gmail.com>
2019-11-05 01:35:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,8 +14,8 @@ end
 def not_safe?(speed)
   #binding.pry
   (40...60).include?speed ? true : false
-  speed >= 60 ? true : false
-  speed <= 40 ? true : false
+  speed >= 60 ? true : false || speed <= 40 ? true : false
+  
 end
 	
 
